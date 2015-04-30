c510cef6cdd0287caa84f816c2755969a1e40e95
Atsushi Hagiwara <hagiwara@cnsinc.jp>
2015-04-30 10:11:09 +0900

    index_test.php master

---
+++ index_test.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="globalsign-domain-verification" content="v20lX8iyX8tS2n3-48H2uoNNfUs-RFC2Y7yNboM214" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>CHIVAS REGAL.jp / TOP</title>
+<meta name="description" content="#" />
+<meta name="keywords" content="#" />
+<link href="css/common.css" rel="stylesheet" type="text/css" />
+<link href="css/index.css" rel="stylesheet" type="text/css" />
+<script language="JavaScript" type="text/javascript" src="plugin/iepngfix_tilebg.js"></script>
+<script type="text/javascript" src="js/swfobject.js"></script>
+<style type="text/css">
+<!--
+ img, div, input, dl ,dd, h2{ behavior: url("plugin/iepngfix.htc") }
+-->
+</style>
+<script type="text/javascript" src="js/rollover.js"></script>
+</head>
+<body>
+<div id="header">	
+	<div id="header-inside" class="clearfix">
+		<h1><a href="index.html"><img src="img/logo_chivas.png" alt="CHIVAS LIVE WITH CHIVALRY" class="iepngfix" /></a></h1>
+		<p><a href="http://chivas.com/" target="_blank"><img src="img/chivas_logo2.png" alt="CHIVAS オフィシャルサイト" class="iepngfix" /></a></p>
+	</div>
+</div>
+	<div id="main-container"><!--一灰色背景背景指定-->
+		<div id="main-container-in"><!--影部分背景指定-->
+			<div id="main-cont"><!--模様部分背景指定-->
+				<div id="main-cont-in"><!--コンテンツエリアWidth決め-->
+					<div id="top-flaaria" style="margin-left:8px; ">
+						<div id="flasharea">
+							<a href="/mizunara/"><img src="images/ChivasRegalJp_Web_top.jpg" alt="シーバスリーガル ミズナラ" width="970" height="330" /></a>
+						</div>
+					</div>
+	
+<div id="cont-left" class="clearfix">
+						<h2><img src="img/cv_ttl_news.png" alt="NEWS" /></h2>
+						<dl>
+							<dt style="margin-top:10px;"><span style="color:#C90;">NEW</span> <a href="/mizunara/bar-dining.php">クーポンキャンペーンはご好評につき、延長が決定しました！ 期間限定でMIZUNARA一杯を無料で楽しめます。</a></dt>
+							<dt style="margin-top:10px;"><span style="color:#C90;">NEW</span> <a href="http://www.pen-online.jp/special/chivasregalmizunara/" onClick="trackOutboundLink('http://www.pen-online.jp/special/chivasregalmizunara/');" target="_blank">Pen OnlineにてMIZUNARA連載特集を掲載中。第2回目が公開になりました。</a></dt>
+							<dt style="margin-top:10px;"><a href="/mizunara/">MIZUNARA キャンペーンが開始しました！リニューアルしたサイトはこちら</a></dt>
+							<dt style="margin-top:10px;"><a href="/masters2015/">シーバス リーガルが主催するバーテンダーを対象にした2つのカクテルコンペティション（シーバス ミズナラ マスターズとシーバス マスターズ）の参加者を募集します。締切は4月12日（日）消印有効です。</a></dt>
+							<dt style="margin-top:10px;">『<a href="/gsc/award/2015/">シーバスリーガル18年ゴールドシグネチャー・アワード2015 presented by GOETHE</a>』、『<a href="/gsc/fund/2014/">シーバスブラザーズ・ヤングアントレプレナー基金 supported by GOETHE</a>』 の受賞者が決定しました。</dt>
+							<dt style="margin-top:10px;"><a href="/gsc/award/2015/notice.html">シーバスリーガル18年ゴールドシグネチャー・アワード2015 presented by GOETHEの授賞式が2015年1月20日に決定しました。</a></dt>
+							<dt style="margin-top:10px;"><a href="/gsc/fund/2014/notice.html">シーバスブラザーズ・ヤングアントレプレナー基金 Supported by GOETHE」の応募は締め切りました。2014年度の受賞者の発表は 2015年1月20日（火）を予定しております。</a></dt>
+							<dt style="margin-top:10px; "><a href="/mizunara/">新商品『シーバスリーガル ミズナラスペシャル・エディション』<br />2013年10月1日に登場!!</a></dt>
+						</dl>
+					</div>
+
+			<div id="cont-right">
+				<div class="top_bottle_link">
+                <h3><img src="images/top_products_title.png" alt="CHIVAS 製品ラインナップ" width="297" height="39" /></h3>
+                <ul style="width:460px; padding:0 10px;">
+                <li><a href="/newsmizunara/" target="_blank"><img src="images2/bottle4_mizunara_off.png" alt="chivas mizunara" width="113" height="295" /></a></li>
+                <li><a href="http://www.chivas.com/ja/jp/chivas/product/" target="_blank"><img src="images2/bottle4_12_off.png" alt="chivas Regal 12" width="114" height="295" /></a></li>
+                <li><a href="http://www.chivas.com/ja/jp/chivas-18/product/" target="_blank"><img src="images2/bottle4_18_off.png" alt="chivas Regal 18" width="114" height="295" /></a></li>
+                <li><a href="http://www.chivas.com/ja/jp/chivas-25/product/" target="_blank"><img src="images2/bottle4_25_off.png" alt="chivas Regal 25" width="114" height="295" /></a></li>
+                </ul>
+                </div>
+                <a href="/mizunara/bar-dining.php"><img src="images/banners/free2_bnr_off.png" alt="CHIVAS MIZUNARA 一杯無料キャンペーン実施中！" /></a><br />
+                <a href="/gsc/"><img src="images/banners/JPTop_GSCBanner_off.png" alt="GOLD SIGNATURE CHANNEL" /></a><br />
+                <a href="https://www.facebook.com/ChivasRegalJapan" target="_blank"><img src="images/banners/fb_bnr_off.png" alt="シーバスリーガル公式facebook" /></a><br />
+				<?php /*
+				<a href="http://aromaoftokyo.jp/" target="_blank"><img src="images/banners/aot2_campaign_off.png" alt="AROMA of TOKYO" /></a><br />
+				<a href="/x/"><img src="img/btn_xautumn_off.png" alt="CHIVAS X" /></a><br />
+				<a target="_blank" href="https://www.facebook.com/ChivasRegalJapan/app_186102038113035"><img src="images/banners/fb_campaign_off.png" alt="公式Facebookページ" /></a><br />
+				<a href="/gsc/fund/"><img src="img/btn_gofund_off.png" alt="シーバスブラザーズ・ヤングアントレプレナー基金" /></a><br />
+				<a href="/gsc/lounge/session3/notice.html"><img src="images/banners/gsc_3rd_off.png" alt="ビジネスパーソンの読書論" /></a><br />
+				<a href="/chivasx/"><img src="images/banners/chivasx_off.png" alt="CHIVAS X" /></a><br />
+				<a href="/scratch/"><img src="img/btn_scratch_off.png" alt="オンライン限定 CHIVASプレゼントキャンペーン" /></a><br />
+				*/ ?>
+			</div>
+
+			<ul id="bottom-menu" class="clearfix">
+				<li id="bottom-menu1"><a href="http://www.chivas.com/" target="_blank" ><img src="img/b_chivas.png" alt="CHIVAS.com" class="iepngfix" /></a></li>
+				<li id="bottom-menu2"><a href="privacy.html"><img src="img/b_privacy.png" alt="プライバシーポリシー" class="iepngfix" /></a></li>
+				<li id="bottom-menu3"><a href="company.html"><img src="img/b_company.png" alt="会社概要" class="iepngfix" /></a></li>
+			</ul>
+			</div>
+		</div>
+	<div id="footer">
+		<p>
+			<span id="over20"><img src="img/cv_over20.png" alt="お酒は20歳になってから" /></span>
+			<span id="copy"><img src="img/copyrights.png" alt="(C)CHIVAS REGAL All rights reserved." /></span>
+		</p>
+	</div>
+</div>
+</div>
+
+<script type="text/javascript" src="/js/ga.js"></script>
+<script>
+/**
+* Function that tracks a click on an outbound link in Google Analytics.
+* This function takes a valid URL string as an argument, and uses that URL string
+* as the event label.
+*/
+var trackOutboundLink = function(url) {
+	ga('send', 'event', 'outbound', 'click', url);
+}
+</script>
+
+</body>
+</html>
